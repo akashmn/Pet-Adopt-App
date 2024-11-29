5403429ea4ad34f1753d4c1f89553a5bb9d3994b
Akash M Nandan <akash003nandan@gmail.com>
2024-11-30 02:11:23 +0530

    clerk auth set [captcha error]

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,40 @@
+import * as SecureStore from 'expo-secure-store'
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      const item = await SecureStore.getItemAsync(key)
+      if (item) {
+        console.log(`${key} was used 🔐 \n`)
+      } else {
+        console.log('No values stored under key: ' + key)
+      }
+      return item
+    } catch (error) {
+      console.error('SecureStore get item error: ', error)
+      await SecureStore.deleteItemAsync(key)
+      return null
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value)
+    } catch (err) {
+      return
+    }
+  },
+}
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  )
+}
 
 export default function RootLayout() {
 
@@ -9,9 +44,17 @@ export default function RootLayout() {
     "outfit-bold": require("../assets/fonts/Outfit-Bold.ttf"),
   });
   
-  return <Stack> 
-    <Stack.Screen name="index"/>
-    <Stack.Screen name="login/index" options={{headerShown:false}}/>
-    
-  </Stack>;
+  return (
+    <ClerkProvider 
+      tokenCache={tokenCache}
+      publishableKey={publishableKey}
+    >
+      <Stack> 
+        <Stack.Screen name="index"/>
+        <Stack.Screen name="login/index" options={{headerShown:false}}/>
+        <Stack.Screen name="home"/>
+        
+      </Stack>
+    </ClerkProvider>
+  );
 }
--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,8 +1,46 @@
 import { View, Text, Pressable } from 'react-native'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Image } from 'react-native'
+import * as WebBrowser from 'expo-web-browser'
+import { Link } from 'expo-router'
+import { useOAuth } from '@clerk/clerk-expo'
+import * as Linking from 'expo-linking'
+
+export const useWarmUpBrowser = () => {
+    React.useEffect(() => {
+      // Warm up the android browser to improve UX
+      // https://docs.expo.dev/guides/authentication/#improving-user-experience
+      void WebBrowser.warmUpAsync()
+      return () => {
+        void WebBrowser.coolDownAsync()
+      }
+    }, [])
+  }
+  
+WebBrowser.maybeCompleteAuthSession()
+  
 
 export default function index() {
+
+    useWarmUpBrowser()
+
+    const { startOAuthFlow } = useOAuth({ strategy: 'oauth_google' })
+
+    const onPress = useCallback(async () => {
+        try {
+          const { createdSessionId, signIn, signUp, setActive } = await startOAuthFlow({
+            redirectUrl: Linking.createURL('/home', { scheme: 'myapp' }),
+          })
+    
+          if (createdSessionId) {
+          } else {
+            // Use signIn or signUp for next steps such as MFA
+          }
+        } catch (err) {
+          console.error('OAuth error', err)
+        }
+      }, [])
+
   return (
     <View style={{
         backgroundColor: "#fff",
@@ -35,7 +73,9 @@ export default function index() {
             Lets adopt a  pet which you like and make there life happy again.
         </Text>
 
-        <Pressable style={{
+        <Pressable 
+        onPress={onPress}
+        style={{
             padding: 14,
             backgroundColor: "#E8B20E",
             width: "100%",
